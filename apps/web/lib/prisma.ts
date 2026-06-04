17564e17efb57547a15df53165339f74d3761225
Xue YANG <xuey648@gmail.com>
2026-06-04 15:30:47 +0200

    feat/favorite-public-events-and-activities

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -6,6 +6,20 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
+type PublicEventFavoriteDelegate = {
+  count: (...args: any[]) => Promise<number>;
+  create: (...args: any[]) => Promise<unknown>;
+  delete: (...args: any[]) => Promise<unknown>;
+  findMany: (...args: any[]) => Promise<unknown[]>;
+  findUnique: (...args: any[]) => Promise<unknown>;
+};
+
+export function getPublicEventFavoriteDelegate() {
+  return (prisma as PrismaClient & {
+    publicEventFavorite?: PublicEventFavoriteDelegate;
+  }).publicEventFavorite;
+}
+
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
